354fd8d853534a7aff8b6e86851cd1ccc52c9319
Fernandez Mateo <mateo.fernandez@etu.umonptellier.fr>
2022-02-10 19:55:35 +0100

    ajout d'un message d'erreur au login

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,21 @@
 async function submit() {
   let login = document.querySelector('input[name="login"]#code-input').value;
   let password = document.querySelector('input[name="password"]#code-input').value;
+  if (login === '') login = 'default';
+  if (password === '') password = 'default';
+
   console.log(`login: ${login}, password: ${password}`);
   let req = await fetch(`./checkLogin.php?login=${login}&password=${password}`);
   if (req.ok) {
     let response = await req.text();
-    if (response !== 'error') {
+    console.log(response);
+    if (response === 'error') {
+      let errorPopup = document.createElement('div');
+      errorPopup.id = 'errorMsg';
+      errorPopup.innerHTML = '<span>❌</span><span>Mauvais login ou mot de passe</span>';
+      document.querySelector('#login-form').appendChild(errorPopup);
+      setTimeout(errorPopup.remove, 2000);
+    } else {
       document.querySelector('#main').innerHTML = response;
     }
   }
