139eae3cc38a9741195219cef2055f63d63687c1
Rotashin <mateo.fernandez@etu.umonptellier.fr>
2022-02-12 02:27:36 +0100

    changement message de fin + php to html

--- assets/app.js
+++ assets/app.js
@@ -5,17 +5,17 @@ async function submit() {
   if (login === '') login = 'default';
   if (password === '') password = 'default';
 
-  console.log(`login: ${login}, password: ${password}`);
+  // console.log(`login: ${login}, password: ${password}`);
   let req = await fetch(`./checkLogin.php?login=${login}&password=${password}`);
   if (req.ok) {
     let response = await req.text();
-    console.log(response);
+    // console.log(response);
     if (response === 'error') {
       let errorPopup = document.createElement('div');
       errorPopup.id = 'errorMsg';
       errorPopup.innerHTML = '<span>❌</span><span>Mauvais login ou mot de passe</span>';
       document.querySelector('#login-form').appendChild(errorPopup);
-      setTimeout(errorPopup.remove, 2000);
+      setTimeout(() => errorPopup.remove(), 2000);
     } else {
       document.querySelector('#main').innerHTML = response;
     }
@@ -25,6 +25,17 @@ async function submit() {
 // DASHBOARD
 function deleteIA() {
   document.querySelector('#delete-button').remove();
-  document.querySelector('#res-msg')
-      .textContent = 'Nice ! Bien ouèj vous avez sauvé la Terre ✅';
+  let loadingImg = document.createElement('img');
+  loadingImg.src = 'assets/loading.gif';
+  let resDiv = document.querySelector('#res-msg');
+  resDiv.appendChild(loadingImg);
+  setTimeout(() => {
+    loadingImg.remove();
+    let textRes = document.createElement('p');
+    textRes.innerHTML = "Félicitations ! 🎉️<br>Vous pouvez maintenant sortir visiter le reste de l'IUT !";
+    textRes.style.fontSize = '2rem';
+    textRes.style.fontFamily = 'sans-serif';
+    textRes.style.fontWeight = 'bold';
+    resDiv.appendChild(textRes);
+  }, 1000);
 }
